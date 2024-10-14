be7d97c42fbedb90466dcb966c241e160a8b28be
TrinhManhHung <manhung31122004@gmail.com>
2024-10-14 07:53:16 +0700

    #9 hung views3

--- myapp/ulrs.py
+++ myapp/ulrs.py
@@ -6,5 +6,7 @@ from django.conf import settings
 urlpatterns = [
     path('', views.index, name='index'),
     path('home/', views1.home, name='home'),
-    path('truyen_id=<int:id>/', views1.doctruyen, name='doctruyen')
+    path('truyen_id=<int:id>/', views1.doctruyen, name='doctruyen'),
+    path('register/', views3.registerPage, name = 'register'),
+    path('login/', views3.loginPage, name = 'login'),
 ]
--- myapp/views3.py
+++ myapp/views3.py
@@ -1,6 +1,39 @@
 from django.shortcuts import render, redirect
-
+from .forms import RegisterForm, LoginForm
+from .models import Nguoidung
 # Create your views here.
 
 def index(request):
 	return render(request, 'index.html')
+
+
+def registerPage(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Chuyển hướng về login sau khi đăng ký thành công
+    else:
+        form = RegisterForm()
+
+    return render(request, 'register.html', {'form': form})
+
+def loginPage(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            # Lấy dữ liệu từ form
+            ten = form.cleaned_data.get('ten')
+            
+            # Lưu tên người dùng vào session để theo dõi trạng thái đăng nhập
+            request.session['nguoidung'] = ten
+            
+            return redirect('home')  # Chuyển hướng về trang chủ sau khi đăng nhập thành công
+    else:
+        form = LoginForm()
+
+    return render(request, 'login.html', {'form': form})
+
+def dang_xuat(request):
+    request.session.flush()  # Xóa tất cả session
+    return redirect('home')
